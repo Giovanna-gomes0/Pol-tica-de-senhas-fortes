7379a4819f15fb8be7aba23284569da37bc05867
Giovanna-gomes0 <gomesgiovanna232@gmail.com>
2025-04-29 08:51:59 -0300

    Add files via upload

---
+++ src/components/Content.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './Content.css';
+
+function Content(){
+    return(
+        <>
+            <section>
+                <h1>Política de Senhas Fortes:
+                     Criação de senhas seguras para proteger contas.</h1>
+                <br />
+                <br />
+                <br />
+                <br />
+                <h2> O que a Política de Senhas Fortes?
+                <br />
+                <br />
+                A política de senhas fortes é um 
+                <br />conjunto de regras e diretrizes que visam garantir a segurança das contas de usuários, protegendo-as 
+                contra acessos não autorizados e cibercrimes. 
+                <br />Essa política define critérios para a criação de senhas, incentivando a utilização 
+                de senhas complexas, únicas e difíceis de adivinhar. .</h2>
+                <br />
+           
+                <h3> Por que é importante?
+                    <br />
+                    <br />
+                    A importância de uma política de 
+                    <br />senhas fortes reside na proteção das informações pessoais e financeiras contra acesso não autorizado, 
+                    roubo de identidade e violações de dados. 
+                    <br />Senhas fortes dificultam que cibercriminosos as descubram, protegendo assim
+                     contas online, como contas bancárias, e-mails e redes sociais. </h3>
+                
+                     
+                    <br />
+                    <br />
+                    <h4> Onde a Politica de Senhas Fortes é utilizada?
+                    <br />
+                    <br />
+                    1- Contas online:
+                    É fundamental para proteger contas bancárias, e-mails, redes sociais, 
+                    <br />sites de compras e outras plataformas onde você armazena informações pessoais. 
+                     <br />
+                     <br />
+                     2- Redes corporativas:
+                     Empresas implementam políticas de senhas fortes para proteger seus sistemas, 
+                     <br />dados e informações confidenciais de funcionários e clientes. 
+                     <br />
+                    <br />
+                    3- Sistemas de informação:
+                    A política de senhas fortes é um pilar da segurança da informação, ajudando 
+                    <br />a proteger dados sensíveis e a prevenir vazamentos de informações. 
+                    <br />
+                    <br />
+                    <iframe src="https://www.youtube.com/embed/8zcrg23K-M8?si=sKkX0L8yYUaM96LJ" title="YouTube video player" 
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+                    encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <br />
+                    <br />
+                    </h4>
+                     <h5>Desenvolvido por Giovanna Gomes e Raiany Carolina </h5>
+            </section>
+        </>
+    );
+};
+export default Content;
